98766332ceafccf79a9b487189385bb30f19801b
Satyam Kumar Jha <satyambhr2@gmail.com>
2025-06-01 16:06:04 +0530

    port chagned

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -27,5 +27,9 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
+
+  const port = process.env.PORT ?? 8080;
+  await app.listen(port);
+  console.log(`Backend server is running on http://localhost:${port}`);
 }
 bootstrap();
